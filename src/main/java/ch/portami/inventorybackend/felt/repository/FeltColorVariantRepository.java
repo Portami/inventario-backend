5d2de396a6d9ba7fb78e27871fbfb9da05a237c4
Rap22tor <elias.baumi@protonmail.ch>
2026-04-18 11:58:05 +0200

    fix: remove unused imports

--- src/main/java/ch/portami/inventorybackend/felt/repository/FeltColorVariantRepository.java
+++ src/main/java/ch/portami/inventorybackend/felt/repository/FeltColorVariantRepository.java
@@ -1,7 +1,6 @@
 package ch.portami.inventorybackend.felt.repository;
 
 import ch.portami.inventorybackend.felt.entity.FeltColorVariant;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeltColorVariantRepository extends JpaRepository<FeltColorVariant, Long> {
--- src/main/java/ch/portami/inventorybackend/felt/repository/FeltVariantRepository.java
+++ src/main/java/ch/portami/inventorybackend/felt/repository/FeltVariantRepository.java
@@ -3,7 +3,6 @@ package ch.portami.inventorybackend.felt.repository;
 import ch.portami.inventorybackend.felt.entity.Felt;
 import ch.portami.inventorybackend.felt.entity.FeltVariant;
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
