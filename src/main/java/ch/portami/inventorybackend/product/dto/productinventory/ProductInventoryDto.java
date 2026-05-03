4cce618e4f9a70cd8aece168612a4099bd3e26d6
Stephan2002 <64665894+Stephan2002@users.noreply.github.com>
2026-05-03 22:26:25 +0200

    Updated ProductInventoryDto to use boxed types instead of primitives

--- src/main/java/ch/portami/inventorybackend/product/dto/productinventory/ProductInventoryDto.java
+++ src/main/java/ch/portami/inventorybackend/product/dto/productinventory/ProductInventoryDto.java
@@ -5,13 +5,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Type representing the amount of a product variant available in a specific storage location.")
 public record ProductInventoryDto(
         @Schema(description = "The ID of the storage this entry belongs to")
-        long storageId,
+        Long storageId,
 
         @Schema(description = "The name of the storage this entry belongs to")
         String storageName,
 
         @Schema(description = "The amount of the product variant available in this storage")
-        int quantity
+        Integer quantity
 ) {
 
 }
