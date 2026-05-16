d6818d2a563f1faa5c427337b10627e1b26c127f
Stephan2002 <64665894+Stephan2002@users.noreply.github.com>
2026-05-16 15:44:33 +0200

    Added endpoints for querying scans

--- src/main/java/ch/portami/inventorybackend/stocktake/felt/api/FeltStocktakeScanController.java
+++ src/main/java/ch/portami/inventorybackend/stocktake/felt/api/FeltStocktakeScanController.java
@@ -3,12 +3,15 @@ package ch.portami.inventorybackend.stocktake.felt.api;
 import ch.portami.inventorybackend.stocktake.felt.dto.scan.CreateFeltStocktakeScanDto;
 import ch.portami.inventorybackend.stocktake.felt.dto.scan.FeltStocktakeScanDto;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/api/stocktakes/{stocktakeId}/scans")
 public class FeltStocktakeScanController {
@@ -16,6 +19,18 @@ public class FeltStocktakeScanController {
     @PostMapping
     public ResponseEntity<FeltStocktakeScanDto> createFeltStocktakeScan(@PathVariable Long stocktakeId,
             @RequestBody @Valid CreateFeltStocktakeScanDto createFeltStocktakeScanDto) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    @GetMapping("/{scanId}")
+    public ResponseEntity<FeltStocktakeScanDto> getFeltStocktakeScanById(@PathVariable Long stocktakeId,
+            @PathVariable Long scanId) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<FeltStocktakeScanDto>> getAllFeltStocktakeScans(@PathVariable Long stocktakeId,
+            @RequestParam(required = false) Long storageId) {
         throw new RuntimeException("Not implemented yet");
     }
 
