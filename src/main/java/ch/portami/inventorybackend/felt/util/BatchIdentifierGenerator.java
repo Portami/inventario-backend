9304cc1738aa42e7903e89dee67057c8c983cd78
Elia Vitali <elia.vitali.code@proton.me>
2026-05-17 17:06:35 +0200

    Make magic number to constant

--- src/main/java/ch/portami/inventorybackend/felt/util/BatchIdentifierGenerator.java
+++ src/main/java/ch/portami/inventorybackend/felt/util/BatchIdentifierGenerator.java
@@ -4,12 +4,15 @@ import java.util.Random;
 
 public final class BatchIdentifierGenerator {
 
+    public static final int IDENTIFIER_LENGTH = 8;
+    public static final String IDENTIFIER_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     private BatchIdentifierGenerator() {}
 
     public static String createIdentifier(Long id) {
         Random random = new Random(id);
-        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder sb = new StringBuilder(8);
+        String chars = IDENTIFIER_CHARACTERS;
+        StringBuilder sb = new StringBuilder(IDENTIFIER_LENGTH);
         for (int i = 0; i < 8; i++) {
             sb.append(chars.charAt(random.nextInt(chars.length())));
         }
