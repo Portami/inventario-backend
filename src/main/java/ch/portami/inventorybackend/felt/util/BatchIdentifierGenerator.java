a18069349c63602ca3e832ff49d652236437c8a2
Elia Vitali <elia.vitali.code@proton.me>
2026-05-17 18:16:37 +0200

    Upgrade for Steven

--- src/main/java/ch/portami/inventorybackend/felt/util/BatchIdentifierGenerator.java
+++ src/main/java/ch/portami/inventorybackend/felt/util/BatchIdentifierGenerator.java
@@ -1,21 +1,38 @@
 package ch.portami.inventorybackend.felt.util;
 
 import java.util.Random;
+import java.util.stream.Collectors;
 
+/**
+ * Utility class for generating deterministic batch identifiers.
+ * <p>
+ * Each identifier is a fixed-length alphanumeric string derived from a
+ * numeric ID. The generation is deterministic: the same input ID will
+ * always produce the same identifier.
+ */
 public final class BatchIdentifierGenerator {
 
+    /** The length of each generated identifier. */
     public static final int IDENTIFIER_LENGTH = 8;
+
+    /** The character pool used to compose identifiers. */
     public static final String IDENTIFIER_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     private BatchIdentifierGenerator() {}
 
+    /**
+     * Creates a deterministic identifier from the given ID.
+     * <p>
+     * The same input ID will always produce the same identifier,
+     * as the ID is used to seed the random number generator.
+     *
+     * @param id the seed used to generate the identifier
+     * @return a string of length {@value #IDENTIFIER_LENGTH} composed
+     *         of characters from {@link #IDENTIFIER_CHARACTERS}
+     */
     public static String createIdentifier(Long id) {
-        Random random = new Random(id);
-        String chars = IDENTIFIER_CHARACTERS;
-        StringBuilder sb = new StringBuilder(IDENTIFIER_LENGTH);
-        for (int i = 0; i < 8; i++) {
-            sb.append(chars.charAt(random.nextInt(chars.length())));
-        }
-        return sb.toString();
+        return new Random(id).ints(IDENTIFIER_LENGTH, 0, IDENTIFIER_CHARACTERS.length())
+                     .mapToObj(i -> String.valueOf(IDENTIFIER_CHARACTERS.charAt(i)))
+                     .collect(Collectors.joining());
     }
 }
